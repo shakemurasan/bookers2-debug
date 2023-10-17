ec4286a434fd6e0f8d852e6a841b4d705d5069d7
Yasuhiro Matsumura <ym.works1985@gmail.com>
2023-10-17 17:20:12 +0000

    redirect books when other user try to edit book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
 
   def show
     @book = Book.find(params[:id])
@@ -22,10 +23,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    should_be_book_owner!(@book)
   end
 
   def update
     @book = Book.find(params[:id])
+    should_be_book_owner!(@book)
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -43,5 +46,11 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def should_be_book_owner!(book)
+    if book.user != current_user
+      redirect_to books_path
+    end
   end
 end
